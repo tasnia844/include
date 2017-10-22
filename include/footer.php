c61697b3632c13f97181a336fca3eef9b1d7fc53
tasnia844 <tasnia.fardous@gmail.com>
2017-10-22 20:27:52 +0600

    my inclide

---
+++ include/footer.php
@@ -0,0 +1,150 @@
+<div class="product mt-5">
+						<h1 class="mb-4">All products</h1>
+						<div class="row">
+						  <div class="col-md-4 mt-4">
+							<div class="card">
+							  <img src="img/laptop.png" alt="" class="card-img-top">
+							  <div class="card-body">
+								<h4 class="card-title">mobile</h4>
+								<div class="card-text">
+								  <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								  consequat. duis aute irure dolor in reprehenderit in voluptate velit esse
+								  cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat cupidatat non
+								  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								  <a class="btn btn-primary" href="product.html">know more</a>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						  <div class="col-md-4 mt-4">
+							<div class="card">
+							  <img src="img/laptop.png" alt="" class="card-img-top">
+							  <div class="card-body">
+								<h4 class="card-title">mobile</h4>
+								<div class="card-text">
+								  <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								  consequat. duis aute irure dolor in reprehenderit in voluptate velit esse
+								  cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat cupidatat non
+								  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								  <a class="btn btn-primary" href="product.html">know more</a>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						  <div class="col-md-4 mt-4">
+							<div class="card">
+							  <img src="img/laptop.png" alt="" class="card-img-top">
+							  <div class="card-body">
+								<h4 class="card-title">mobile</h4>
+								<div class="card-text">
+								  <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								  consequat. duis aute irure dolor in reprehenderit in voluptate velit esse
+								  cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat cupidatat non
+								  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								  <a class="btn btn-primary" href="product.html">know more</a>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						</div>
+						<div class="row">
+						  <div class="col-md-4 mt-4">
+							<div class="card">
+							  <img src="img/laptop.png" alt="" class="card-img-top">
+							  <div class="card-body">
+								<h4 class="card-title">Mobile</h4>
+								<div class="card-text">
+								  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								  <a class="btn btn-primary" href="product.html">Know more</a>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						  <div class="col-md-4 mt-4">
+							<div class="card">
+							  <img src="img/laptop.png" alt="" class="card-img-top">
+							  <div class="card-body">
+								<h4 class="card-title">Mobile</h4>
+								<div class="card-text">
+								  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								  <a class="btn btn-primary" href="product.html">Know more</a>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						  <div class="col-md-4 mt-4">
+							<div class="card">
+							  <img src="img/laptop.png" alt="" class="card-img-top">
+							  <div class="card-body">
+								<h4 class="card-title">Mobile</h4>
+								<div class="card-text">
+								  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								  <a class="btn btn-primary" href="product.html">Know more</a>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						</div>
+					  </div>
+
+
+
+
+
+					  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+							  <div class="carousel-item active">
+								<img class="d-block w-100" src="img/girl2.jpg" alt="First slide">
+							  </div>
+							  <div class="carousel-item">
+								<img class="d-block w-100" src="img/girl2.jpg" alt="Second slide">
+							  </div>
+							  <div class="carousel-item">
+								<img class="d-block w-100" src="img/girl2.jpg" alt="Third slide">
+							  </div>
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+							  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							  <span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+							  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+							  <span class="sr-only">Next</span>
+							</a>
+						  </div>
+
+
+
+						  <form>
+								<div class="form-group">
+								  <label for="exampleInputEmail1">Email address</label>
+								  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+								  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+								</div>
+								<div class="form-group">
+								  <label for="exampleInputPassword1">Password</label>
+								  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+								</div>
+								<div class="form-check">
+								  <label class="form-check-label">
+									<input type="checkbox" class="form-check-input">
+									Check me out
+								  </label>
+								</div>
+								<button type="submit" class="btn btn-primary">Submit</button>
+							  </form>
+							  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117538.52720036735!2d91.3403824637263!3d22.984123201488583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37536810dae74897%3A0xbb00500c594464ee!2sFeni+Sadar+Upazila%2C+Bangladesh!5e0!3m2!1sen!2s!4v1508680613889" width="1100" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
+		
+
+  
---
+++ include/header.php
@@ -0,0 +1,47 @@
+
+	
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="#"><img src="img/icon.png"</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+			  
+				<div class="collapse navbar-collapse high" id="navbarSupportedContent">
+				  <ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+					  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+					  <a class="nav-link" href="product.html">product</a>
+					</li>
+					<li class="nav-item dropdown">
+					  <a class="nav-link dropdown-toggle" href="about.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						about
+					  </a>
+					  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">address</a>
+						<a class="dropdown-item" href="#">email</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">contact</a>
+					  </div>
+					</li>
+					<li class="nav-item">
+					  <a class="nav-link disabled" href="profile.html">profile</a>
+					</li>
+				  </ul>
+				  <form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				  </form>
+				</div>
+			  </nav>
+
+			  <div class="jumbotron header color">
+					<h1 class="display-3 ">Hello, world!</h1>
+					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					<hr class="my-4">
+					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<p class="lead">
+					  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+					</p>
+				  </div>
